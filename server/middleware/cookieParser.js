39cea9951a6c787d46ce049d11ad2b3def492f9f
kage1414 <kylejohnson92294@gmail.com>
2021-03-09 20:34:56 -0500

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,14 @@
 const parseCookies = (req, res, next) => {
+  if (req.headers.cookie) {
+    let cookie = {};
+    let splitCookies = req.headers.cookie.split('; ');
+    splitCookies.forEach((idx) => {
+      let tuple = idx.split('=');
+      cookie[tuple[0]] = tuple[1];
+    });
+    req.cookies = cookie;
+  }
+  next();
 };
 
 module.exports = parseCookies;
